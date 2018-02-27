f3f5944c3267214a70a8fd1608ba3028cad405ff
deltawhiskey91L <kmckenna@mymail.mines.edu>
2018-02-26 17:53:44 -0700

    Added Departure Functions

--- PengRobinson.py
+++ PengRobinson.py
@@ -6,30 +6,182 @@ import numpy as np
 import UnitConverter as unit
 import QRDecomposition as qr
 
-# LOGGING
+
 root_path = os.path.dirname(os.path.realpath(__file__))
-logging.basicConfig(filename=root_path + '/Logs/run.log', level=logging.DEBUG)
-# Logging Levels:
-# CRITICAL
-# ERROR
-# WARNING
-# INFO
-# DEBUG
+runlog = logging.getLogger('runlog')
+alglog = logging.getLogger('alglog')
+
+
+def departure_H(temp, press, temp_crit, press_crit, acentric_factor):
+    """
+    Enthalpy Departure Function using the Peng-Robingson Equation of State
+
+    :param temp: Current Temperature, T (K)
+    :type temp: float
+    :param press: Current Pressure, P (Pa)
+    :type press: float
+    :param temp_crit: Substance Critical Temperature, Tc (K)
+    :type temp_crit: float
+    :param press_crit: Substance Critical Pressure, Pc (Pa)
+    :type press_crit: float
+    :param acentric_factor: Acentric Factor, omega (unitless)
+    :type acentric_factor: float
+    :return: Enthalpy Departure Function, Delta H
+    :rtype: float
+    """
+    alglog.info('Peng-Robinson Enthalpy Departure Function.')
+
+    R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
+    dadt = dadT(temp, temp_crit, press_crit, acentric_factor)
+    a = a_factor(temp, temp_crit, press_crit, acentric_factor)
+    b = b_factor(temp_crit, press_crit)
+
+    vol = volume(temp, press, temp_crit, press_crit, acentric_factor)
+    if vol[2] is np.nan:
+        vol = vol[0]
+    else:
+        vol = vol[2]
+
+    Z = press * vol / (R * temp)
+
+    dH = R * temp * (Z - 1) - (temp * dadt - a)/(2 * np.sqrt(2) * b) * \
+        np.log((Z + (1 - np.sqrt(2)) * b * press / (R * temp))/(Z + (1 + np.sqrt(2)) * b * press / (R * temp)))
+    return dH
+
+
+def departure_S(temp, press, temp_crit, press_crit, acentric_factor):
+    """
+    Entropy Departure Function using the Peng-Robingson Equation of State
+
+    :param temp: Current Temperature, T (K)
+    :type temp: float
+    :param press: Current Pressure, P (Pa)
+    :type press: float
+    :param temp_crit: Substance Critical Temperature, Tc (K)
+    :type temp_crit: float
+    :param press_crit: Substance Critical Pressure, Pc (Pa)
+    :type press_crit: float
+    :param acentric_factor: Acentric Factor, omega (unitless)
+    :type acentric_factor: float
+    :return: Entropy Departure Function, Delta S
+    :rtype: float
+    """
+    alglog.info('Peng-Robinson Entropy Departure Function.')
+
+    R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
+    dadt = dadT(temp, temp_crit, press_crit, acentric_factor)
+    b = b_factor(temp_crit, press_crit)
+
+    vol = volume(temp, press, temp_crit, press_crit, acentric_factor)
+    if vol[2] is np.nan:
+        vol = vol[0]
+    else:
+        vol = vol[2]
+
+    Z = press * vol / (R * temp)
+
+    dS = R * temp * (Z - b * press/(R * temp)) - dadt / (2 * np.sqrt(2) * b) * \
+        np.log((Z + (1 - np.sqrt(2)) * b * press / (R * temp)) / (Z + (1 + np.sqrt(2)) * b * press / (R * temp)))
+    return dS
+
+
+def departure_G(temp, press, temp_crit, press_crit, acentric_factor):
+    """
+    Gibbs Free-Energy Departure Function using the Peng-Robingson Equation of State
+
+    :param temp: Current Temperature, T (K)
+    :type temp: float
+    :param press: Current Pressure, P (Pa)
+    :type press: float
+    :param temp_crit: Substance Critical Temperature, Tc (K)
+    :type temp_crit: float
+    :param press_crit: Substance Critical Pressure, Pc (Pa)
+    :type press_crit: float
+    :param acentric_factor: Acentric Factor, omega (unitless)
+    :type acentric_factor: float
+    :return: Gibbs Free-Energy Departure Function, Delta G
+    :rtype: float
+    """
+    alglog.info('Peng-Robinson Gibbs Free-Energy Departure Function.')
+
+    dS = departure_S(temp, press, temp_crit, press_crit, acentric_factor)
+    dH = departure_H(temp, press, temp_crit, press_crit, acentric_factor)
+
+    return dH - temp * dS
+
+
+def departure_U(temp, press, temp_crit, press_crit, acentric_factor):
+    """
+    Internal Energy Departure Function using the Peng-Robingson Equation of State
+
+    :param temp: Current Temperature, T (K)
+    :type temp: float
+    :param press: Current Pressure, P (Pa)
+    :type press: float
+    :param temp_crit: Substance Critical Temperature, Tc (K)
+    :type temp_crit: float
+    :param press_crit: Substance Critical Pressure, Pc (Pa)
+    :type press_crit: float
+    :param acentric_factor: Acentric Factor, omega (unitless)
+    :type acentric_factor: float
+    :return: Internal Energy Departure Function, Delta G
+    :rtype: float
+    """
+    alglog.info('Peng-Robinson Internal Energy Departure Function.')
+
+    vol = volume(temp, press, temp_crit, press_crit, acentric_factor)
+    if vol[2] is np.nan:
+        vol = vol[0]
+    else:
+        vol = vol[2]
+    dH = departure_H(temp, press, temp_crit, press_crit, acentric_factor)
+
+    return dH - press * vol
+
+
+def departure_A(temp, press, temp_crit, press_crit, acentric_factor):
+    """
+    Helmholtz Free-Energy Departure Function using the Peng-Robingson Equation of State
+
+    :param temp: Current Temperature, T (K)
+    :type temp: float
+    :param press: Current Pressure, P (Pa)
+    :type press: float
+    :param temp_crit: Substance Critical Temperature, Tc (K)
+    :type temp_crit: float
+    :param press_crit: Substance Critical Pressure, Pc (Pa)
+    :type press_crit: float
+    :param acentric_factor: Acentric Factor, omega (unitless)
+    :type acentric_factor: float
+    :return: Internal Energy Departure Function, Delta G
+    :rtype: float
+    """
+    alglog.info('Peng-Robinson Helmholtz Free-Energy Departure Function.')
+
+    dU = departure_U(temp, press, temp_crit, press_crit, acentric_factor)
+    dS = departure_S(temp, press, temp_crit, press_crit, acentric_factor)
+
+    return dU - temp * dS
 
 
 def pressure(temp, vol, temp_crit, press_crit, acentric_factor):
-    """Pressure from Peng-Robinson Equation of State
-    Inputs:
-        temp: Temperature, T (K)
-        volume: Volume, V (m^3)
-        temp_crit: Critical Temperature, Tc (K)
-        press_crit: Critical Pressure, Pc (Pa)
-        accntric_factor: Acentric factor, omega (unitless)
-    Outputs:
-        press: Pressure, P (Pa)
     """
-    t = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    logging.info('{0} INFO: Calculating pressure from the Peng-Robinson Equation of State.'.format(t))
+    Pressure using the Peng-Robinson Equation of State
+
+    :param temp: Current Temperature, T (K)
+    :type temp: float
+    :param vol: Current Volume, V (m**3)
+    :type vol: float
+    :param temp_crit: Substance Critical Temperature, Tc (K)
+    :type temp_crit: float
+    :param press_crit: Substance Critical Pressure, Pc (Pa)
+    :type press_crit: float
+    :param acentric_factor: Acentric Factor, omega (unitless)
+    :type acentric_factor: float
+    :return: Pressure, P (Pa)
+    :rtype: float
+    """
+    alglog.info('Peng-Robinson Pressure.')
 
     R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
     a = a_factor(temp, temp_crit, press_crit, acentric_factor)
@@ -40,18 +192,24 @@ def pressure(temp, vol, temp_crit, press_crit, acentric_factor):
 
 
 def volume(temp, press, temp_crit, press_crit, acentric_factor):
-    """Volume Peng-Robinson Equation of State
-    Inputs:
-        temp: Temperature, T (K)
-        press: Pressure, P (Pa)
-        temp_crit: Critical Temperature, Tc (K)
-        press_crit: Critical Pressure, Pc (Pa)
-        accntric_factor: Acentric factor, omega (unitless)
-    Outputs:
-        volume: Volume, V (m^3)
     """
-    t = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    logging.info('{0} INFO: Calculating volume from the Peng-Robinson Equation of State.'.format(t))
+    Calculates fluid from from Peng-Robinson Equation of State.
+    If multiphase conditions exist, returns all three volumes.
+
+    :param temp: Current Temperature, T (K)
+    :type temp: float
+    :param pressure: Current Pressure, P (Pa)
+    :type pressure: float
+    :param temp_crit: Substance Critical Temperature, Tc (K)
+    :type temp_crit: float
+    :param press_crit: Substance Critical Pressure, Pc (Pa)
+    :type press_crit: float
+    :param acentric_factor: Acentric Factor, omega (unitless)
+    :type acentric_factor: float
+    :return: Volume, V (m**3)
+    :rtype np.array
+    """
+    alglog.info('Peng-Robinson Volume.')
 
     R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
     a = a_factor(temp, temp_crit, press_crit, acentric_factor)
@@ -85,8 +243,8 @@ def dPdV(temp, press, temp_crit, press_crit, acentric_factor):
     Outputs:
         dpdv: Pressure Derivative with Respect to Volume at Constant Temperature, dP/dV_T (Pa/(mol-m^3))
     """
-    t = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    logging.info('{0} INFO: Calculating volume from the Peng-Robinson Equation of State.'.format(t))
+
+    alglog.info('Peng-Robinson dP/dV.')
 
     R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
     a = a_factor(temp, temp_crit, press_crit, acentric_factor)
@@ -113,8 +271,8 @@ def dPdT(temp, press, temp_crit, press_crit, acentric_factor):
     Outputs:
         dpdv: Pressure Derivative with respect to Temperature at constant Volume, dP/dT_V (Pa/K)
     """
-    t = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    logging.info('{0} INFO: Calculating volume from the Peng-Robinson Equation of State.'.format(t))
+
+    alglog.info('Peng-Robinson dP/dT.')
 
     R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
     b = b_factor(temp_crit, press_crit)
@@ -128,12 +286,14 @@ def dPdT(temp, press, temp_crit, press_crit, acentric_factor):
 
 
 def dadT(temp, temp_crit, press_crit, acentric_factor):
+    alglog.info('Peng-Robinson da/dT.')
     a = a_factor(temp, temp_crit, press_crit, acentric_factor)
     k = kappa(acentric_factor)
     return - k * a / ((1 + k * (1 - np.sqrt(temp/temp_crit)))*np.sqrt(temp * temp_crit))
 
 
 def ddadT2(temp, temp_crit, press_crit, acentric_factor):
+    alglog.info('Peng-Robinson dda/dT**2.')
     R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
     ac = 0.45723553 * R ** 2 * temp_crit ** 2 / press_crit
     k = kappa(acentric_factor)
@@ -141,6 +301,7 @@ def ddadT2(temp, temp_crit, press_crit, acentric_factor):
 
 
 def a_factor(temp, temp_crit, press_crit, acentric_factor):
+    alglog.info('Peng-Robinson a-factor.')
     R = 8.314459848  # Gas Constant: m^3 Pa mol^-1 K^-1
     ac = 0.45723553 * R**2 * temp_crit**2 / press_crit
     k = kappa(acentric_factor)
@@ -159,6 +320,7 @@ def b_factor(temp_crit, press_crit):
     :return: Value of b (m**3)
     :rtype: float
     """
+    alglog.info('Peng-Robinson b-factor.')
     R = 8.314459848  # Gas Constant: m**3 Pa / (mol K)
 
     if press_crit == 0:
@@ -167,7 +329,6 @@ def b_factor(temp_crit, press_crit):
         raise ZeroDivisionError
 
     return 0.077796074 * R * temp_crit / press_crit
-
 
 
 def kappa(acentric_factor):
@@ -179,4 +340,5 @@ def kappa(acentric_factor):
     :return: Value of kappa
     :rtype: float
     """
+    alglog.info('Peng-Robinson kappa.')
     return 0.37464 + 1.54226 * acentric_factor - 0.26992 * acentric_factor ** 2
